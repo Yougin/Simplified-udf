3867cc10c724eb49515dddde2a93bd7a26af2b42
CantSayWhoIam <no@no.no>
2019-05-03 11:29:20 +0200

    Clean test #2.

--- app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/GroupByDateTest.kt
+++ app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/GroupByDateTest.kt
@@ -10,8 +10,6 @@ class GroupByDateTest {
 
   private lateinit var groupByDate: GroupByDate
 
-  private val fakeBooks get() = listOf(book1, book2, book3, book4, book5, book6).shuffled()
-
   @Before fun setUp() {
     groupByDate = GroupByDate()
   }
@@ -47,11 +45,13 @@ class GroupByDateTest {
     assertThat(books[Year(2019)]?.get(Title("27"))).contains(book6)
   }
 
-  private val book1 get() = Book("id2", "w = 27, y = 2014", "B", LocalDate.of(2014, 7, 2), "url")
-  private val book2 get() = Book("id5", "w = 52, y = 2017", "B", LocalDate.of(2017, 12, 31), "url")
-  private val book3 get() = Book("id1", "w = 30, y = 2018", "A", LocalDate.of(2018, 7, 23), "url")
-  private val book4 get() = Book("id3", "w = 27, y = 2018", "C", LocalDate.of(2018, 7, 3), "url")
-  private val book5 get() = Book("id4", "w = 27, y = 2019", "E", LocalDate.of(2019, 7, 3), "url")
-  private val book6 get() = Book("id5", "w = 27, y = 2019", "F", LocalDate.of(2019, 7, 4), "url")
+  private val fakeBooks get() = listOf(book1, book2, book3, book4, book5, book6).shuffled()
+
+  private val book1 get() = Book("id2", "aName", "", LocalDate.of(2014, 7, 2), "")
+  private val book2 get() = Book("id5", "bName", "", LocalDate.of(2017, 12, 31), "")
+  private val book3 get() = Book("id1", "aName", "", LocalDate.of(2018, 7, 23), "")
+  private val book4 get() = Book("id3", "cName", "", LocalDate.of(2018, 7, 3), "")
+  private val book5 get() = Book("id4", "zName", "", LocalDate.of(2019, 7, 3), "")
+  private val book6 get() = Book("id5", "zName", "", LocalDate.of(2019, 7, 4), "")
 
 }
