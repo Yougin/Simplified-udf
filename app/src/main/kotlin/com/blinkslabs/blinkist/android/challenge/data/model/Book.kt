8804a8b1a97f2a6c8dda198d7c029aef0e5b50bf
CantSayWhoIam <no@no.no>
2019-05-03 08:14:17 +0200

    Clean it up #2.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/model/Book.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/model/Book.kt
@@ -3,8 +3,15 @@ package com.blinkslabs.blinkist.android.challenge.data.model
 import org.threeten.bp.LocalDate
 
 
-data class Book(val id: String,
-                val name: String,
-                val author: String,
-                val publishDate: LocalDate,
-                val coverImageUrl: String)
+data class Book(
+    val id: String,
+    val name: String,
+    val author: String,
+    val publishDate: LocalDate,
+    val coverImageUrl: String
+) {
+
+  val publishYear get() = publishDate.year
+
+  override fun toString(): String = name
+}
--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/Sorter.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/Sorter.kt
@@ -9,21 +9,19 @@ inline class Title(val title: String)
 
 class WeeklySorter {
 
-  fun sort(books: List<Book>): Map<Int, Map<Title, List<Book>>> {
-    val booksByYear: Map<Int, List<Book>> = books
-        .sortedBy { it.publishDate.year }
-        .groupBy { it.publishDate.year }
-
-    val result: LinkedHashMap<Int, Map<Title, List<Book>>> = LinkedHashMap()
-    booksByYear.forEach { (k, v) ->
-      result[k] = v.groupBy {
-        val weekNumber = getWeekNumber(it.publishDate).toString()
-        Title(weekNumber)
+  fun sort(books: List<Book>): Map<Int, Map<Title, List<Book>>> =
+      with(LinkedHashMap<Int, Map<Title, List<Book>>>()) {
+        books
+            .sortedBy { book -> book.publishYear }
+            .groupBy { book -> book.publishYear }
+            .forEach { (year, booksThisYear) ->
+              this[year] = booksThisYear.groupBy {
+                val weekNumber = getWeekNumber(it.publishDate).toString()
+                Title(weekNumber)
+              }
+            }
+        this
       }
-    }
-
-    return result
-  }
 
   private fun getWeekNumber(publishDate: LocalDate): Int {
     val week = WeekFields.of(DayOfWeek.MONDAY, 1)
