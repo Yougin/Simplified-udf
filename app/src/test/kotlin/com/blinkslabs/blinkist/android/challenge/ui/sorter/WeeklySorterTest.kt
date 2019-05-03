c0262afbf9f0fbf911b32ac4c4964895ac67a87d
CantSayWhoIam <no@no.no>
2019-05-03 07:23:57 +0200

    Add a concrete test for list of books published in a week.

--- app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/WeeklySorterTest.kt
+++ app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/WeeklySorterTest.kt
@@ -30,12 +30,30 @@ class WeeklySorterTest {
     2019={27=[2019 also week 27, y = 2019]}} */
   }
 
-  private fun getBooks() = listOf(
-      Book("id2", "w = 27, y = 2014", "B", LocalDate.of(2014, 7, 2), "url"),
-      Book("id5", "w = 27, y = 2019", "D", LocalDate.of(2017, 12, 31), "url"),
-      Book("id1", "w = 30, y = 2018", "A", LocalDate.of(2018, 7, 23), "url"),
-      Book("id3", "w = 27, y = 2018", "C", LocalDate.of(2018, 7, 3), "url"),
-      Book("id4", "w = 27, y = 2019", "D", LocalDate.of(2019, 7, 3), "url")
-  ).shuffled()
+  @Test fun `should sort books by week`(){
+    val books = getBooks()
+
+    val sortedBooks = sorter.sort(books)
+
+    println(sortedBooks)
+    assertThat(sortedBooks[2014]?.get(27)).isEqualTo(listOf(book1))
+    // TODO-eugene 52?
+    assertThat(sortedBooks[2017]?.get(53)).isEqualTo(listOf(book2))
+    assertThat(sortedBooks[2018]?.get(30)).isEqualTo(listOf(book3))
+    assertThat(sortedBooks[2018]?.get(27)).isEqualTo(listOf(book4))
+
+    assertThat(sortedBooks[2019]?.get(27)).hasSize(2)
+    assertThat(sortedBooks[2019]?.get(27)).contains(book5)
+    assertThat(sortedBooks[2019]?.get(27)).contains(book6)
+  }
+
+  private fun getBooks() = listOf(book1, book2, book3, book4, book5, book6).shuffled()
+
+  private val book1 get() = Book("id2", "w = 27, y = 2014", "B", LocalDate.of(2014, 7, 2), "url")
+  private val book2 get() = Book("id5", "w = 52, y = 2017", "D", LocalDate.of(2017, 12, 31), "url")
+  private val book3 get() = Book("id1", "w = 30, y = 2018", "A", LocalDate.of(2018, 7, 23), "url")
+  private val book4 get() = Book("id3", "w = 27, y = 2018", "C", LocalDate.of(2018, 7, 3), "url")
+  private val book5 get() = Book("id4", "w = 27, y = 2019", "D", LocalDate.of(2019, 7, 3), "url")
+  private val book6 get() = Book("id4", "w = 27, y = 2019", "D", LocalDate.of(2019, 7, 4), "url")
 
 }
