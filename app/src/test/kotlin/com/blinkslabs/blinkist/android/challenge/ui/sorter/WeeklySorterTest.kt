c7a305e9f7659f09df3a9bd9033bcd3af752aabd
CantSayWhoIam <no@no.no>
2019-05-03 07:28:17 +0200

    Temporary save current output.

--- app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/WeeklySorterTest.kt
+++ app/src/test/kotlin/com/blinkslabs/blinkist/android/challenge/ui/sorter/WeeklySorterTest.kt
@@ -25,9 +25,7 @@ class WeeklySorterTest {
     assertThat(sortedBooks[2018]).hasSize(2)
     assertThat(sortedBooks[2019]).hasSize(1)
 
-    /* {2014={27=[1st book week 27, y = 2014]},
-    2018={30=[book week 30, y = 2018], 27=[2nd book week 27, y = 2018]},
-    2019={27=[2019 also week 27, y = 2019]}} */
+    println(sortedBooks)
   }
 
   @Test fun `should sort books by week`(){
@@ -49,11 +47,18 @@ class WeeklySorterTest {
 
   private fun getBooks() = listOf(book1, book2, book3, book4, book5, book6).shuffled()
 
+  /*
+  2014={27=[w = 27, y = 2014]},
+  2017={53=[w = 52, y = 2017]},
+  2018={27=[w = 27, y = 2018], 30=[w = 30, y = 2018]},
+  2019={27=[w = 27, y = 2019, w = 27, y = 2019]}
+  */
+
   private val book1 get() = Book("id2", "w = 27, y = 2014", "B", LocalDate.of(2014, 7, 2), "url")
   private val book2 get() = Book("id5", "w = 52, y = 2017", "D", LocalDate.of(2017, 12, 31), "url")
   private val book3 get() = Book("id1", "w = 30, y = 2018", "A", LocalDate.of(2018, 7, 23), "url")
   private val book4 get() = Book("id3", "w = 27, y = 2018", "C", LocalDate.of(2018, 7, 3), "url")
-  private val book5 get() = Book("id4", "w = 27, y = 2019", "D", LocalDate.of(2019, 7, 3), "url")
-  private val book6 get() = Book("id4", "w = 27, y = 2019", "D", LocalDate.of(2019, 7, 4), "url")
+  private val book5 get() = Book("id4", "w = 27, y = 2019", "E", LocalDate.of(2019, 7, 3), "url")
+  private val book6 get() = Book("id5", "w = 27, y = 2019", "F", LocalDate.of(2019, 7, 4), "url")
 
 }
