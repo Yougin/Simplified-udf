c1a4677a331625d8f9ea29824f7f4736dedd2f85
CantSayWhoIam <no@no.no>
2019-05-04 20:45:47 +0200

    Add factory method for db instantiation.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/storage/database/BooksDatabase.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/storage/database/BooksDatabase.kt
@@ -1,6 +1,8 @@
 package com.blinkslabs.blinkist.android.challenge.data.storage.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookDao
 import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookEntity
@@ -9,4 +11,22 @@ import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookEntity
 abstract class BooksDatabase : RoomDatabase() {
 
   abstract fun bookDao(): BookDao
+
+  companion object {
+    private var INSTANCE: BooksDatabase? = null
+
+    fun getInstance(context: Context): BooksDatabase? {
+      if (INSTANCE == null) {
+        synchronized(BooksDatabase::class) {
+          INSTANCE = Room.databaseBuilder(context.applicationContext,
+                                          BooksDatabase::class.java, "books.db").build()
+        }
+      }
+      return INSTANCE
+    }
+
+    fun destroyInstance() {
+      INSTANCE = null
+    }
+  }
 }
