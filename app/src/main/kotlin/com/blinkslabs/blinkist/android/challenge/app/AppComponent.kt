69ac4a8b551ad0daabfc2578f03c4af778bb061b
CantSayWhoIam <no@no.no>
2019-05-04 09:54:58 +0200

    Add UtilsModule.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/app/AppComponent.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/app/AppComponent.kt
@@ -3,11 +3,12 @@ package com.blinkslabs.blinkist.android.challenge.app
 import com.blinkslabs.blinkist.android.challenge.data.DataModule
 import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.BooksActivityComponent
 import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.rootview.UiModule
+import com.blinkslabs.blinkist.android.challenge.util.UtilsModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [DataModule::class, UiModule::class])
+@Component(modules = [DataModule::class, UiModule::class, UtilsModule::class])
 interface AppComponent {
 
   fun inject(app: BlinkistChallengeApplication)
