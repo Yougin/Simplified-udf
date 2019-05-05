ee1b79e45eb4be6021afbed2a420300248217c3f
CantSayWhoIam <no@no.no>
2019-05-05 13:24:00 +0200

    Set "replace" as a strategy for conflict.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/book/entity/BookDao.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/book/entity/BookDao.kt
@@ -2,13 +2,14 @@ package com.blinkslabs.blinkist.android.challenge.data.book.entity
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.reactivex.Observable
 
 @Dao
 interface BookDao {
 
-  @Insert
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
   fun insertBook(book: BookEntity): Long
 
   @Query("SELECT * FROM BookEntity")
