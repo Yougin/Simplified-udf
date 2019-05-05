6a0596c235d66b954e8b5741f82963e734d46d33
CantSayWhoIam <no@no.no>
2019-05-05 22:55:32 +0200

    update comment.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/featureswitch/FeatureRepository.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/featureswitch/FeatureRepository.kt
@@ -3,8 +3,8 @@ package com.blinkslabs.blinkist.android.challenge.data.featureswitch
 import com.blinkslabs.blinkist.android.challenge.domain.featurewitch.GroupByWeeklyFeature
 import io.reactivex.subjects.PublishSubject
 
-// This one is made so just to gain time, ideally it should be implemented
-// the same way BookRepository is done - exposing an observable data set.
+// This one is made so just to save time, ideally it should be implemented
+// the same way BookRepository is done - by exposing an API which represent an observable data set.
 object FeatureRepository {
 
   val weeklyFeature: PublishSubject<GroupByWeeklyFeature> = PublishSubject.create()
