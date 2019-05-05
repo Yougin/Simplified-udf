b2a92bce6d9e64f479254e72aa3ad697fd5aef21
CantSayWhoIam <no@no.no>
2019-05-05 14:03:29 +0200

    Implement TypeConverter fof LocalDate.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/book/entity/BookEntity.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/book/entity/BookEntity.kt
@@ -3,14 +3,30 @@ package com.blinkslabs.blinkist.android.challenge.data.book.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.io.Serializable
-
+import androidx.room.TypeConverter
+import org.threeten.bp.LocalDate
 
 @Entity
 data class BookEntity(
     @PrimaryKey(autoGenerate = false) var id: String,
     @ColumnInfo(name = "name") var name: String,
     @ColumnInfo(name = "author") var author: String,
-//    @ColumnInfo(name = "publish_date") var publishDate: LocalDate,
+    @ColumnInfo(name = "publish_date") var publishDate: LocalDate,
     @ColumnInfo(name = "cover_image_url") var coverImageUrl: String
-) : Serializable
+)
+
+object BooksTypeConverters {
+
+  @TypeConverter
+  @JvmStatic
+  fun toOffsetDateTime(value: Long?): LocalDate? {
+    return if (value != null) LocalDate.ofEpochDay(value) else null
+  }
+
+  @TypeConverter
+  @JvmStatic
+  fun fromOffsetDateTime(date: LocalDate?): Long? {
+    return date?.toEpochDay()
+
+  }
+}
--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/database/BooksDatabase.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/database/BooksDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookDao
 import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookEntity
+import com.blinkslabs.blinkist.android.challenge.data.book.entity.BooksTypeConverters
 
 @Database(entities = [BookEntity::class], version = 1, exportSchema = false)
+@TypeConverters(BooksTypeConverters::class)
 abstract class BooksDatabase : RoomDatabase() {
 
   abstract fun bookDao(): BookDao
