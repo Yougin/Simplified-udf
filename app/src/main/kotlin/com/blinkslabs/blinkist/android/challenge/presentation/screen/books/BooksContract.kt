a89def2b23f87b44060dc7d0baead354729e3cf6
CantSayWhoIam <no@no.no>
2019-05-05 23:05:30 +0200

    clean up.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/BooksContract.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/BooksContract.kt
@@ -3,16 +3,21 @@ package com.blinkslabs.blinkist.android.challenge.presentation.screen.books
 import com.blinkslabs.blinkist.android.challenge.domain.book.model.Books
 import com.blinkslabs.blinkist.android.challenge.domain.featurewitch.GroupByWeeklyFeature
 
+sealed class BooksIntent {
+
+  object InitialIntent : BooksIntent()
+
+  object ForceUpdateIntent : BooksIntent()
+}
+
 sealed class BooksViewState {
+
   object InFlight : BooksViewState()
+
   data class DataFetched(
-      val books: Books, val weeklyFeature: GroupByWeeklyFeature
+      val books: Books,
+      val weeklyFeature: GroupByWeeklyFeature
   ) : BooksViewState()
 
   data class Error(val throwable: Throwable) : BooksViewState()
-}
-
-sealed class BooksIntent {
-  object InitialIntent : BooksIntent()
-  object ForceUpdateIntent : BooksIntent()
 }
