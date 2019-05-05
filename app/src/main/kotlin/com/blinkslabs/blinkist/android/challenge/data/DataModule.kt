1051999143600f18d5b03ee1e9040c9ccbea0bbb
CantSayWhoIam <no@no.no>
2019-05-05 13:23:43 +0200

    Declare database dependencies.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/DataModule.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/data/DataModule.kt
@@ -1,17 +1,29 @@
 package com.blinkslabs.blinkist.android.challenge.data
 
 import android.content.Context
+import androidx.room.Room
 import com.blinkslabs.blinkist.android.challenge.data.book.BooksApi
 import com.blinkslabs.blinkist.android.challenge.data.book.MockBooksApi
+import com.blinkslabs.blinkist.android.challenge.data.book.entity.BookDao
+import com.blinkslabs.blinkist.android.challenge.data.database.BooksDatabase
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class DataModule(private val app: Context) {
+class DataModule(private val context: Context) {
 
   @Provides
   @Singleton
   fun providesBooksApi(booksApi: MockBooksApi): BooksApi = booksApi
+
+  @Provides
+  @Singleton
+  fun providesRoomDatabase(): BooksDatabase =
+      Room.databaseBuilder(context, BooksDatabase::class.java, "books_db").build()
+
+  @Provides
+  @Singleton
+  fun providesBooksDao(booksDatabase: BooksDatabase): BookDao = booksDatabase.bookDao()
 
 }
