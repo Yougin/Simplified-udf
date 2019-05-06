018ea67be20f07b4a8fb9775baa871b9aab56fe4
CantSayWhoIam <no@no.no>
2019-05-06 14:26:01 +0200

    Implement WeekTitleItemDelegate.kt

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/delegate/WeekTitleItemDelegate.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/delegate/WeekTitleItemDelegate.kt
@@ -4,19 +4,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.blinkslabs.blinkist.android.challenge.R
 import com.blinkslabs.blinkist.android.challenge.presentation.common.adapter.ItemDelegate
+import kotlinx.android.synthetic.main.item_week_title.view.*
 
 data class WeekTitle(val text: String)
 
 class WeekTitleItemDelegate : ItemDelegate<WeekTitle> {
-  override val associatedDataType: Class<WeekTitle>
-    get() = TODO("not implemented")
 
-  override fun create(layoutInflater: LayoutInflater, parent: ViewGroup?): RecyclerView.ViewHolder {
-    TODO("implement")
-  }
+  override val associatedDataType: Class<WeekTitle> = WeekTitle::class.java
+
+  override fun create(layoutInflater: LayoutInflater, parent: ViewGroup?): RecyclerView.ViewHolder =
+      ItemDelegate.Holder(layoutInflater.inflate(R.layout.item_week_title, parent, false))
 
   override fun bind(view: View, data: WeekTitle) {
+    view.item_week_title_text_view.text = data.text
   }
 
 }
