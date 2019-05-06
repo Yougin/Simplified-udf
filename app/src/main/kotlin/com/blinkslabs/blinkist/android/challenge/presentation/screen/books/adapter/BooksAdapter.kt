75356d427ac7bb0f1db70049523cf0a2569ff841
CantSayWhoIam <no@no.no>
2019-05-06 14:30:47 +0200

    Remove tmp code.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/BooksAdapter.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/BooksAdapter.kt
@@ -4,7 +4,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.blinkslabs.blinkist.android.challenge.domain.book.model.Books
 import com.blinkslabs.blinkist.android.challenge.domain.featurewitch.GroupByWeeklyFeature
 import com.blinkslabs.blinkist.android.challenge.presentation.common.adapter.FlexibleAdapterImpl
-import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.adapter.delegate.*
+import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.adapter.delegate.AlphabetTitleItemDelegate
+import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.adapter.delegate.BookCardItemDelegate
+import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.adapter.delegate.WeekTitleItemDelegate
+import com.blinkslabs.blinkist.android.challenge.presentation.screen.books.adapter.delegate.YearTitleItemDelegate
 import javax.inject.Inject
 
 interface BooksAdapter {
@@ -23,7 +26,7 @@ class BooksAdapterImpl @Inject constructor() : FlexibleAdapterImpl(
 ), BooksAdapter {
 
   override fun setBooks(books: Books, weeklyFeature: GroupByWeeklyFeature) {
-    items = convertToAdapterData(books, weeklyFeature).filterIsInstance(BookCard::class.java)
+    items = convertToAdapterData(books, weeklyFeature)
     notifyDataSetChanged()
   }
 
