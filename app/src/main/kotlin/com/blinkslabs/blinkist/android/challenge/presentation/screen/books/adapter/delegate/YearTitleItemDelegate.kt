91be2e5ed0a367e40fc8eeb2e9266fd1657f4a1b
CantSayWhoIam <no@no.no>
2019-05-06 14:24:14 +0200

    Implement YearTitleItemDelegate.

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/delegate/YearTitleItemDelegate.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/presentation/screen/books/adapter/delegate/YearTitleItemDelegate.kt
@@ -4,20 +4,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.blinkslabs.blinkist.android.challenge.R
 import com.blinkslabs.blinkist.android.challenge.presentation.common.adapter.ItemDelegate
+import kotlinx.android.synthetic.main.item_year_title.view.*
 
 data class YearTitle(val text: String)
 
 class YearTitleItemDelegate : ItemDelegate<YearTitle> {
 
   override val associatedDataType: Class<YearTitle>
-    get() = TODO("not implemented")
+    get() = YearTitle::class.java
 
-  override fun create(layoutInflater: LayoutInflater, parent: ViewGroup?): RecyclerView.ViewHolder {
-    TODO("not implemented")
-  }
+  override fun create(layoutInflater: LayoutInflater, parent: ViewGroup?): RecyclerView.ViewHolder =
+      ItemDelegate.Holder(layoutInflater.inflate(R.layout.item_year_title, parent, false))
 
   override fun bind(view: View, data: YearTitle) {
+    view.item_year_title_text_view.text = data.text
   }
 
 }
